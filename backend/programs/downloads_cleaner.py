15a6c4ea65446b99cf3f9497ae22e9b931089218
Salamun8 <52977980+Salamun8@users.noreply.github.com>
2020-11-14 17:42:56 -0500

    Update downloads_cleaner.py

--- backend/programs/downloads_cleaner.py
+++ backend/programs/downloads_cleaner.py
@@ -4,17 +4,22 @@ import datetime
 
 home = os.path.expanduser('~')
 
-x_days = input("Enter x number of days so that the program deletes all files older than x days: ")
+xdays = float(input("Enter x number of days so that the program deletes all files older than x days: "))
 
 today = datetime.datetime.today()
 
 path = home + '/Downloads/'
 filesearch = os.listdir(path)
 
+for f in filesearch:
+    print(f)
+
+print("These are all your files in the downloads folder")
+
 for p in filesearch:
     mtime = datetime.datetime.fromtimestamp(os.path.getmtime(home+'/Downloads/'+p))
     duration = today - mtime
-    if duration.days < x_days:
+    if duration.days > xdays and p.endsWith(".dmg" or ".exe") != True:
         os.remove(home+'/Downloads/'+p)
         print(p+" has been removed")
         print(mtime)
